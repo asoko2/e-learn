0a2ab8fac473163b9fe012af7f2daded740344ce
Anang Hariyanto N <sokoanang@gmail.com>
2022-03-18 17:21:45 +0700

    Fix error student_id

--- index.php
+++ index.php
@@ -6,15 +6,18 @@ session_start();
 if (!isset($_SESSION['name'])) {
     header('location: sign-in.php');
 }
-$survey = mysqli_query($conn, "SELECT * FROM survey_result where student_id = '{$_SESSION['student_id']}'");
-$survey_row = mysqli_num_rows($survey);
-if ($survey_row == 1) {
-    $_SESSION['survey_taken'] = true;
-    $level = mysqli_fetch_array($survey, MYSQLI_ASSOC);
-    $_SESSION['level'] = $level['level_result'];
-    $_SESSION['levels'] = $survey;
-} else {
-    $_SESSION['survey_taken'] = false;
+
+if ($_SESSION['level_user'] == 3) {
+    $survey = mysqli_query($conn, "SELECT * FROM survey_result where student_id = '{$_SESSION['student_id']}'");
+    $survey_row = mysqli_num_rows($survey);
+    if ($survey_row == 1) {
+        $_SESSION['survey_taken'] = true;
+        $level = mysqli_fetch_array($survey, MYSQLI_ASSOC);
+        $_SESSION['level'] = $level['level_result'];
+        $_SESSION['levels'] = $survey;
+    } else {
+        $_SESSION['survey_taken'] = false;
+    }
 }
 
 
